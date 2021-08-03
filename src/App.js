02cc4fe68918b6c09ab1ba98a1a8c93ce1c048e7
slama-wael <slama_wael2@hotmail.com>
2021-08-03 11:14:40 +0100

    premier commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class App extends React.Component {
+
+  state = {
+    Profil: {
+      fullName: "Slama Wael",
+      bio: "Ingénieur en Génie électrique Informatique industriel avec une experience de 2 ans dans le domaine travaux électrique & électricité de bâtiments et 3 ans dans le domaine d'entretien & maintenance industrielle",
+      imgSrc: "/photo3.jpg",
+      profession: "chef d'entreprise 'slama traveaux' opperant dans le domaine d'entretien & maintenance industrielle",
+    },
+    show: true,
+    count: 0,
+  }
+
+  handelhide=()=>this.setState({show : !this.state.show, count:0})
+  componentDidMount () {
+    setInterval ( () => {
+      this.setState(count => ({count:this.state.count +1}));
+    } , 500 )
+  } ;
+
+
+  render() {
+    return(
+      <div className="mb-0">
+        <button onClick={() => this.handelhide()}>{this.state.show ? "dont show profile" : "show profile"}</button>
+        {this.state.show ? (
+        <>
+        <br></br>
+        <img className="mb-4" src={this.state.Profil.imgSrc} alt="myphoto" />
+        <h1 className="mb-1">{this.state.Profil.fullName}</h1>
+        <h2 className="mb-2">{this.state.Profil.bio}</h2>
+        <h2 className="mb-3">{this.state.Profil.profession}</h2>
+        </>) : null}
+        {this.state.show ? 
+        <p>{this.state.count}</p>
+        : null }
+        </div>
+    )
+  }
+ }
 
 export default App;
